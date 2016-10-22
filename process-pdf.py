0093e51db72b5275e29e0d5c1226c29fcfadb8b3
Paul D Barham <paulb@savagesoft.com>
2016-10-22 00:00:49 -0500

    Refactored code

--- process-pdf.py
+++ process-pdf.py
@@ -4,6 +4,15 @@ import requests
 import json
 import csv
 
+pdf_file_path = 'data/'
+pdf_base_name = 'K2012 Report of Sale'
+pdf_file_name = pdf_file_path + pdf_base_name + '.pdf'
+
+pdf_start_page = 2
+pdf_end_page = 387
+
+output_csv = open(pdf_base_name + '.csv', 'w')
+csv_writer = csv.writer(output_csv, dialect='excel')
 
 def cleannone(val):
     if val is None:
@@ -12,38 +21,29 @@ def cleannone(val):
 
 # K2012 Report of Sale.pdf     starts on page 3
 
-pdfFileObj = open('data/K2012 Report of Sale.pdf', 'rb')
+pdfFileObj = open(pdf_file_name, 'rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-print ( pdfReader.numPages )
 
-page = 2
+if pdfReader.numPages < pdf_end_page:
+    pdf_end_page = pdfReader.numPages
 
-parcelPattern = re.compile(r'PARCEL NUMBER: ([K0123456789-]*) LEGAL DESCRIPTION: (.+)(\d{2}-\d{3}-\d{2}-\d{2}-\d{2}-\d{1}-\d{2}-\d{3})')
+
+# parcelPattern = re.compile(r'PARCEL NUMBER: ([K0123456789-]*) LEGAL DESCRIPTION: (.+)(\d{2}-\d{3}-\d{2}-\d{2}-\d{2}-\d{1}-\d{2}-\d{3})')
 amountsPattern = re.compile(r'([\({][0-9\.,\$]+[\)}])')
-
-# judgment and ERMITA PORTALES, 5228 SAIDA AVENUE, KANSAS CITY, MO 64123, being the highest
 
 parcelPattern = re.compile(r'PARCEL NUMBER: ([K0123456789-]*) LEGAL DESCRIPTION: (.+)(\d{2}-\d{3}-\d{2}-\d{2}-\d{2}-\d{1}-\d{2}-\d{3}) .+ judgment and (.+), being the highest.+to the said (.+),\s+at\s+said')
 
-output_csv = open('K2012 Report of Sale.csv', 'w')
 
-csv_writer = csv.writer(output_csv, dialect='excel')
-
-header = {'Parcel Number', 'Legal Description', 'APN', 'PDF Name Address', 'PDF Name', 'PDF Address'}
-
+header = ['Parcel Number', 'Legal Description', 'APN', 'PDF Name Address', 'PDF Name', 'PDF Address']
 
 csv_writer.writerows( [header] )
 
-for i in range(2, 387):
+for i in range(pdf_start_page, pdf_end_page):
     pageObj = pdfReader.getPage(i)
 
     text =  pageObj.extractText() 
 
     text =  text.replace(chr(10),' ') 
-
-#    print ( text )
-
-    # PARCEL NUMBER: K2012-01030
 
     ret = parcelPattern.search( text )
 
@@ -60,11 +60,8 @@ for i in range(2, 387):
 
         address = addressPattern.search( name_and_address )
 
-
         if address is not None:
             address = addressPattern.search( name_and_address ).groups(0)
-
-
             ret.append(address[0])
         else:
             ret.append( '' )
