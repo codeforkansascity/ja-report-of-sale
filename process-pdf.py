d6a4d9ff6cf930bb8f31729f74048c846aa8bad3
Paul D Barham <paulb@savagesoft.com>
2016-10-28 10:40:09 -0500

    Added date sold, source, and page number

--- process-pdf.py
+++ process-pdf.py
@@ -9,7 +9,7 @@ def cleannone(val):
         val = '';
     return val
 
-def strip_data( pdf_file_path, pdf_base_name, pdf_start_page, pdf_end_page ):
+def strip_data( pdf_file_path, pdf_base_name, pdf_start_page, pdf_end_page, date_sold ):
     pdf_file_name = pdf_file_path + pdf_base_name + '.pdf'
 
     print ( 'Processing ' + pdf_file_name )
@@ -25,14 +25,9 @@ def strip_data( pdf_file_path, pdf_base_name, pdf_start_page, pdf_end_page ):
     if pdfReader.numPages < pdf_end_page:
         pdf_end_page = pdfReader.numPages
     
-    
-    # parcelPattern = re.compile(r'PARCEL NUMBER: ([K0123456789-]*) LEGAL DESCRIPTION: (.+)(\d{2}-\d{3}-\d{2}-\d{2}-\d{2}-\d{1}-\d{2}-\d{3})')
-    amountsPattern = re.compile(r'([\({][0-9\.,\$]+[\)}])')
-    
     parcelPattern = re.compile(r'PARCEL NUMBER: ([K0123456789-]*) LEGAL DESCRIPTION: (.+)(\d{2}-\d{3}-\d{2}-\d{2}-\d{2}-\d{1}-\d{2}-\d{3}) .+ judgment and (.+), being the highest.+to the said (.+),\s+at\s+said')
-    
-    
-    header = ['Parcel Number', 'Legal Description', 'APN', 'PDF Name Address', 'PDF Name', 'PDF Address', 'Situs Address', 'URL']
+
+    header = ['Parcel Number', 'Legal Description', 'APN', 'Owner Name Address', 'Date Sold', 'longitude', 'latitude', 'Situs Address','URL', 'Source', 'Page No.']
     
     csv_writer.writerows( [header] )
     
@@ -49,34 +44,43 @@ def strip_data( pdf_file_path, pdf_base_name, pdf_start_page, pdf_end_page ):
             ret = ret.groups()
             ret = list( ret )
         
-            name_and_address = ret[3]
-            name = ret[4]
-    
-            searchString = name + ', (.+)'
-    
-            addressPattern = re.compile(searchString)
-    
-            address = addressPattern.search( name_and_address )
-    
-            if address is not None:
-                address = addressPattern.search( name_and_address ).groups(0)
-                ret.append(address[0])
-            else:
-                ret.append( '' )
-    
+            del ret[4]
 
-            single_line_address = address_api ( ret[2] )
+            ret.append ( date_sold );
+
+            longitude = ''
+            latitude = ''
+            single_line_address = ''
+
+            api_info = address_api ( ret[2] )
+
+
+
+            if api_info is not None:
+                api_info = list( api_info )
+                longitude = api_info[0]
+                latitude = api_info[1]
+                single_line_address = api_info[2]
+
+            ret.append( longitude )
+            ret.append( latitude )
             ret.append( single_line_address )
 
             ret.append( 'http://maps.jacksongov.org/PropertyReport/propertyReport.cfm?pid=' + ret[2] )
 
+            ret.append( pdf_file_name )
+           
+            page_number = i
+
+            ret.append( page_number )
+
             csv_writer.writerows( [ ret ]  )
+
+    print ( 'Last page processed ' + i )
 
 def address_api( p ):
 
     parcel_number = 'JA' + p.replace("-", "")
-
-    # http://dev-api.codeforkc.org//address-attributes/V0/10001%20N%20CHERRY%20DR%2C?city=Kansas%20City&state=mojj
 
     url = 'http://dev-api.codeforkc.org/address-attributes-county-id/V0/' + parcel_number + '?city=Kansas%20City&state=MO'
 
@@ -98,17 +102,16 @@ def address_api( p ):
 
         address_api_data = jData['data']
         
-        single_line_address = cleannone(address_api_data['single_line_address'])
-        county_situs_address = cleannone(address_api_data['county_situs_address'])
-        longitude = address_api_data['longitude']
-        latitude = address_api_data['latitude']
+        single_line_address = cleannone(address_api_data['single_line_address']) + ' ' + cleannone(address_api_data['zip'])
+        longitude = cleannone(address_api_data['longitude'])
+        latitude = cleannone(address_api_data['latitude'])
 
-        return single_line_address
+        return longitude, latitude, single_line_address
 
 
-pdf_end_page = 387
+pdf_end_page = 390
 
-strip_data( 'data/', 'K2012 Report of Sale', 2, 387)
+strip_data( 'data/', 'K2012 Report of Sale', 2, pdf_end_page, '2013-08-28' )
 
 #strip_data( 'data/kc report of sale K2011/', 'kc report of sale vol. 1', 2, 10)
 #strip_data( 'data/kc report of sale K2011/', 'kc report of sale vol. 1-1', 2, 50)
